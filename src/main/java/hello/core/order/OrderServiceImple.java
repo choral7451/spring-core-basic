3fc647eb472d5ceb9d8583b6437d16f14f3e7d30
Daniel Im <chorales@naver.com>
2023-03-04 17:18:02 +0900

    싱글톤 컨테이너

--- src/main/java/hello/core/order/OrderServiceImple.java
+++ src/main/java/hello/core/order/OrderServiceImple.java
@@ -22,4 +22,9 @@ public class OrderServiceImple implements OrderService {
 
     return new Order(memberId, itemName, itemPrice, discountPrice);
   }
+
+  //테스트 용도
+  public MemberRepository getMemberRepository() {
+    return memberRepository;
+  }
 }
