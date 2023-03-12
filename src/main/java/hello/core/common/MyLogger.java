533250f7afd1df01fb389518140388816c1e3e61
Daniel Im <chorales@naver.com>
2023-03-13 01:03:58 +0900

    Completion

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -3,15 +3,18 @@ package hello.core.common;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Scope(value = "request")
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
   private String uuid;
   private String requestUrl;
 
-  public MyLogger(String requestUrl) {
+  public void setRequestUrl(String requestUrl) {
     this.requestUrl = requestUrl;
   }
 
